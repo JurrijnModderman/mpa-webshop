86072fe2a7c7d4951f4ef0c1a7dc2f214c7fd098
JurrijnModderman <99053654@mydavinci.nl>
2021-09-06 14:13:21 +0200

    Changed navbar styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,9 +13,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 @if (Auth::user())
 <a class="navbar-brand" href="{{route('home')}}">Home</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
   <li class="logout"><a href="{{ url('/logout') }}" class="menu-btn">Logout</a></li>
   @else
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
